5ed0e1b5f00ac23c017019ca0285f5388ddd8cfd
Taylor Williams <taylordwilliams90@gmail.com>
2019-12-11 16:04:18 -0700

    update app

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let list = ["Start 261 project", "finish workout", "make dinner"];
+var list = ["Start 261 project", "finish workout", "make dinner"];
 
 function getlist(){
     //var list = ["Start 261 project", "finish workout", "make dinner"];
@@ -14,8 +14,14 @@ function getlist(){
 function addItem(){
     item = document.getElementById("list-item").value;
     list.push(item);
+    console.log(list);
+    var item ="";
+    for (var i = 0; i < list.length; i++){
+        item += "<li onclick='crossout()' class='listitems' id='" + i + "'>" + list[i] + "</li>";
+    }
+    document.getElementById("todoList").innerHTML = item;
 }
 
 function crossout(){
-
+    
 }
