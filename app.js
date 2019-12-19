0816212713f841567bf15c97cf2232bee89671f6
Taylor Williams <taylordwilliams90@gmail.com>
2019-12-18 20:41:37 -0700

    toda ajax and json

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var list = [];
 function getlist(){
     //var list = ["Start 261 project", "finish workout", "make dinner"];
 
-    
     var item ="";
     for (var i = 0; i < list.length; i++){
         item += "<li class='listitems'  onclick='addStyle(this)' id='list'>" + list[i] + "</li>";
@@ -13,6 +12,11 @@ function getlist(){
     document.getElementById("todoList").innerHTML = item;
 
 
+    $.getJSON("/data.json", function(result){
+        $.each(result, function(i, field){
+          $("#example").append(field + " <br/> ");
+        });
+      });
 }
 
 function addItem(){
@@ -28,4 +32,30 @@ function addItem(){
 
    function addStyle(list){
     $(list).addClass("cross");
+}
+
+
+function loadDoc() {
+    var xhttp;
+    if (window.XMLHttpRequest) {
+        // code for modern browsers
+        xhttp = new XMLHttpRequest();
+        } else {
+        // code for IE6, IE5
+        xhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            var jsonObj = JSON.parse(http_request.responseText);
+
+            // jsonObj variable now contains the data structure and can
+            // be accessed as jsonObj.name and jsonObj.country.
+            for(var i = 0; i < json.length; i++) {
+            document.getElementById("Name").innerHTML = jsonObj.name;
+            document.getElementById("Class").innerHTML = jsonObj.message;
+            }
+        }
+    };
+    xhttp.open("GET", "data.json", true);
+    xhttp.send();
 }
